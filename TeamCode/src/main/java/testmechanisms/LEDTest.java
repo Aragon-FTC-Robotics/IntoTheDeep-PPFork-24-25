98c68251fa331c905d549f6ca9e483a8814c610d
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-01-13 22:12:55 -0800

    led bugfix

--- TeamCode/src/main/java/testmechanisms/LEDTest.java
+++ TeamCode/src/main/java/testmechanisms/LEDTest.java
@@ -1,13 +1,14 @@
 package testmechanisms;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Config
 public class LEDTest {
     Servo LED;
     public static double light = 0;
     public void init(HardwareMap hm) {
-        LED = hm.get(Servo.class, "LED");
+        LED = hm.get(Servo.class, "shiny");
     }
     public void Loop() {
         LED.setPosition(light);
--- TeamCode/src/main/java/testmechanisms/LEDTestOpmode.java
+++ TeamCode/src/main/java/testmechanisms/LEDTestOpmode.java
@@ -1,9 +1,28 @@
 package testmechanisms;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Bar", group = "testing")
-public class LEDTestOpmode {
-    public LEDTest LED = new LEDTest();
+@TeleOp(name = "Led", group = "testing")
+public class LEDTestOpmode extends LinearOpMode {
+    MultipleTelemetry dashboardTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    LEDTest LED = new LEDTest();
+    @Override
+    public void waitForStart() {
+        super.waitForStart();
+    }
+    @Override
+    public void runOpMode() {
+        LED.init(hardwareMap);
 
+        waitForStart();
+
+        while(opModeIsActive() && !isStopRequested()) {
+            LED.Loop();
+            dashboardTelemetry.addData("Last commanded position:", "%.3f", LED.LED.getPosition());
+            dashboardTelemetry.update();
+        }
+    }
 }
